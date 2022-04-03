a59c32a94552ae792eff530a29ec03a195f45527
Kit4Whale <60572662+Kit4Whale@users.noreply.github.com>
2022-04-03 23:52:27 +0300

    Optimisation

--- src/test/java/ru/deliveryClub/DeliveryClubSiteTest.java
+++ src/test/java/ru/deliveryClub/DeliveryClubSiteTest.java
@@ -1,22 +1,16 @@
 package ru.deliveryClub;
 
-import org.checkerframework.checker.units.qual.C;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DeliveryClubSiteTest {
+public class DeliveryClubSiteTest extends WebDriverSettings {
 
     @Test
     public void checkTheSite() {
-        System.setProperty("webdriver.chrome.driver", "/Users/nikitastrahov/Downloads/chromedriver");
-        ChromeDriver driver = new ChromeDriver();
-
         driver.get("https://www.delivery-club.ru/");
 
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Delivery Club — Доставка еды и продуктов"));
-
-        driver.quit();
     }
 }
